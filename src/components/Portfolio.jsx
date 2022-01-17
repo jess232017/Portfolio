a5ebc09f97d133f50b04327ff5f6fef1a52f9953
Jensen Hernández <Jess232016@outlook.com>
2022-01-17 16:13:16 -0600

    minor fixes

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from 'react';
 
-//controls
-import { Flipper, Flipped } from "react-flip-toolkit";
-
 //icons
 import { FaMobile, FaDesktop, FaServer, FaEye } from 'react-icons/fa'
 
