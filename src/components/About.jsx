f5a47832a0f8ed12b19d9d8db7836e4ecc58262f
Jensen Hernández <Jess232016@outlook.com>
2022-01-26 11:20:04 -0600

    fixes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,14 +13,14 @@ const About = () => {
                     <div className="profile">
                         <img className="profile-photo" src="https://res.cloudinary.com/js-media/image/upload/v1642526363/portfolio/carnet/Picture1_ch2yeq.jpg" alt="Jesus Hernández" />
                         <div>
-                            <p className="font-medium"><b>Perfil:</b> Desarrollador de Software</p>
+                            <p className="font-medium"><b>Perfil:</b> Ingeniero en computación</p>
                             <p><b>Idiomas:</b> Español (Nativo), Inglés (Basico)</p>
                         </div>
                     </div>
                     <div className='text'>
                         <p className='m-0'>Hola, soy </p>
                         <h2>Jesús Hernández</h2>
-                        <p> un apasionado de la programación y el desarrollo web, me gusta día a día aprender y mejorar mis habilidades, me especializo en tecnologías frontend, sin embargo siempre estoy dispuesto a adaptarme a nuevos lenguajes, librerías y frameworks. </p>
+                        <p> un apasionado de la programación y el desarrollo web, me gusta día a día aprender y mejorar mis habilidades, me especializo en tecnologías frontend, sin embargo siempre estoy dispuesto a adaptarme a nuevos lenguajes, bibliotecas y frameworks. </p>
                         <p>
                             Actualmente me encuentro estudiando el quinto año de Ingeniería en computación. Me considero una persona responsable, con iniciativa, puntualidad, compromiso y de rápido aprendizaje.
                         </p>
