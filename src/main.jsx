b4fbdd724d704a276020c811b9d94acdfbd99b68
Jensen Hernández <Jess232016@outlook.com>
2022-02-18 09:51:48 -0600

    added: Google Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+// YOUR_OWN_TRACKING_ID
+const TRACKING_ID = "G-HF3KQGFEKF";
+ReactGA.initialize(TRACKING_ID);
 
 ReactDOM.render(
   <React.StrictMode>
